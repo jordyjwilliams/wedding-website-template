ea368e1883df64f16944cb542ee7382416a6b068
Jordy Williams <jordy_williams@hotmail.co.uk>
2026-03-25 00:57:26 +1100

    feat: vite config to ts and use tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), tailwindcss()],
   ssr: {
     // Force Vite to bundle this package through the Svelte plugin
     // rather than letting Node try to load the raw .svelte file directly.
