a5128472ed363d6d8866fbf93ceec0327285b3da
Jordy Williams <jordy_williams@hotmail.co.uk>
2026-03-25 00:16:55 +1100

    feat: remove unnecessary styling

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -145,9 +145,6 @@
             >
               <Icon icon={link.icon} width="22" />
               {link.label}
-              {#if active}
-                <span class="bg-secondary absolute right-4 bottom-2 h-0.5 w-10 rounded-full"></span>
-              {/if}
             </a>
           {/each}
         </nav>
