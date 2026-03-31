bb9f71dc955ac32e19bccb29bc246146aa99072b
Jordy Williams <jordy_williams@hotmail.co.uk>
2026-04-01 02:40:12 +1100

    feat: utility typing for inline links

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export type InlineLinkSegment = {
+  text: string;
+  href: string | null;
+};
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
