2751af9dc246f4a21a024e63db55d07fafd34c25
Jordy Williams <jordy_williams@hotmail.co.uk>
2026-04-03 03:28:00 +1100

    feat: fix warnings

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -96,11 +96,11 @@
                shrink-0 overflow-hidden"
       >
         <SunIcon
-          class="h-[18px] w-[18px] scale-100 rotate-0
+          class="h-4.5 w-4.5 scale-100 rotate-0
                  transition-all duration-300 dark:scale-0 dark:-rotate-90"
         />
         <MoonIcon
-          class="absolute inset-0 m-auto h-[18px] w-[18px] scale-0 rotate-90
+          class="absolute inset-0 m-auto h-4.5 w-4.5 scale-0 rotate-90
                  transition-all duration-300 dark:scale-100 dark:rotate-0"
         />
         <span class="sr-only">Toggle theme</span>
