2a6600f034c9f82f7eb4dc568875f2b781c6327a
Jordy Williams <jordy_williams@hotmail.co.uk>
2026-05-24 23:44:11 +1000

    feat: add RSVP to main page

--- src/routes/our-story/+page.svelte
+++ src/routes/our-story/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-  import { AboutUs, PageWrapper, StorySections } from '$lib/components';
+  import { AboutUs, PageWrapper, StorySections, SectionCta } from '$lib/components';
   import { COPY } from '$lib/content';
 </script>
 
 <PageWrapper backgroundImage="/images/story-bg.webp" backgroundPosition="center top">
   <AboutUs />
-  <StorySections entries={COPY.about.storySections.entries} />
+  <StorySections entries={COPY.about.storySections.entries} title={COPY.about.storySectionsTitle} />
+  <div class="container pb-20">
+    <div class="mx-auto max-w-5xl">
+      <SectionCta
+        text="Ready to celebrate with us?"
+        buttonLabel="RSVP Now →"
+        href="/rsvp"
+        openInNewTab={false}
+        class="animate-fade-in-up"
+      />
+    </div>
+  </div>
 </PageWrapper>
